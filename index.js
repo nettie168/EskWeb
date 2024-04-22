0504682621301932e5cfe4619360770a21d84c13
nettie168 <antoinette.alevropoulos@gmail.com>
2024-04-22 13:55:47 +0100

    featured posts changed

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get("/patreon", (req,res) => {
 
 app.get("/blogs", (req,res) =>{
     const posts = fs.readdirSync(__dirname + "/" +"views" + "/" +"blogs").filter(file => file.endsWith('.ejs'));
-    const featuredPosts = ["What-are-habits__-And-why-are-they-important__","Having-a-bad-day__-Try-these-5-tips","You-are-made-of-stardust"]
+    const featuredPosts = ["What-are-habits__-And-why-are-they-important__","Having-a-bad-day__-Try-these-5-tips","6-Tips-on-doing-tasks-when-you-don’t-want-to"]
 
     res.render("blogsList.ejs", {
         featuredPosts: featuredPosts,
